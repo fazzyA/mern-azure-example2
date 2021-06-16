0fdd5fb724d1a6eae9955951e86035e8b8ffa5ff
fazzyA <faz.pak@gmail.com>
2021-06-17 01:38:07 +0500

    db

--- server/db-conn.js
+++ server/db-conn.js
@@ -5,7 +5,9 @@ const { DB_CONN, DB_USER, DB_PW } = process.env;
 mongoose
   .connect(
     DB_CONN,
-    { auth: { user: DB_USER, password: DB_PW }, useNewUrlParser: true },
+    { auth: { user: DB_USER, password: DB_PW }, 
+    useNewUrlParser: true,
+    useUnifiedTopology: true },
   )
   .then(() => console.log('Succesfully connected to the DB through the power of magic...'))
   .catch(console.error);
